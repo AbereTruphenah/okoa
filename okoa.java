3958714c4dabfc2bfda02e9a99168ffec09f544f
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2023-10-05 15:26:30 +0300

    Add files via upload

---
+++ okoa.java
@@ -0,0 +1,294 @@
+package demoswitch;
+import java.util.Scanner;
+	
+
+public class okoa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice=0;
+		int option;
+		
+		System.out.println("0:Sh20 (30 mins,3hrs)");
+		System.out.println("1:Okoa easy Sh50(50Mins,midnight)");
+		System.out.println("2:Okoa 100");
+		System.out.println("3:Okoa 50");
+		System.out.println("4:Okoa 20(or KSh50,Midnight)");
+		System.out.println("5:Okoa 10");
+		System.out.println("6:Okoa 5");
+		System.out.println("7:Okoa Internet");
+		System.out.println("98:More");
+		System.out.println("");//enter your choice//
+		Scanner input=new Scanner(System.in);
+		choice=input.nextInt();
+		switch(choice) {
+		case 0:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:20");
+			System.out.println("TOTAL DEBT will be:20");			
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("0:BACK 00:HOME");
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			else if(option==2) {
+				System.out.println("Decline");
+			}
+			else if(option==00) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");
+			}
+			else {
+				System.out.println("Invalid choice. Try again.");
+			}
+			
+			break;
+			
+		
+		case 1:
+			System.out.println("Okoa easy Sh50(50Mins,midnight)");
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:50");
+			System.out.println("");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			else if(option==2) {
+				System.out.println("Decline");
+			}
+			else {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");
+			}
+			break;
+		case 2:
+			
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			else if(option==2) {
+				System.out.println("Decline");
+			}
+			else {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");
+			}
+			break;
+			
+		case 3:
+			
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			else if(option==2) {
+				System.out.println("Decline");
+			}
+			else {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");
+			}
+			break;
+		case 4:
+			
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			else if(option==2) {
+				System.out.println("Decline");
+			}
+			else if(option==00) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");
+			}
+			break;
+		case 5:
+			
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			
+			
+			break;		
+case 6:
+			
+			System.out.println("");//enter your option//
+			option=input.nextInt();
+			if(option==0) {
+				System.out.println(":Sh20 (30 mins,3hrs)");
+				System.out.println("Okoa easy Sh50(50Mins,midnight)");
+				System.out.println("Okoa 100");
+				System.out.println("Okoa 50");
+				System.out.println("Okoa 20(or KSh50,Midnight)");
+				System.out.println("Okoa 10");
+				System.out.println("Okoa 5");
+				System.out.println("Okoa Internet");
+				System.out.println("More");					
+			}
+			else if(option==1) {
+				System.out.println("Accept");
+			}
+			
+			
+			break;
+case 7:
+	
+	System.out.println("");//enter your option//
+	option=input.nextInt();
+	if(option==0) {
+		System.out.println(":Sh20 (30 mins,3hrs)");
+		System.out.println("Okoa easy Sh50(50Mins,midnight)");
+		System.out.println("Okoa 100");
+		System.out.println("Okoa 50");
+		System.out.println("Okoa 20(or KSh50,Midnight)");
+		System.out.println("Okoa 10");
+		System.out.println("Okoa 5");
+		System.out.println("Okoa Internet");
+		System.out.println("More");					
+	}
+	else if(option==1) {
+		System.out.println("Accept");
+	}
+	
+	
+	break;
+case 98:
+	
+	System.out.println("");//enter your option//
+	option=input.nextInt();
+	if(option==0) {
+		System.out.println(":Sh20 (30 mins,3hrs)");
+		System.out.println("Okoa easy Sh50(50Mins,midnight)");
+		System.out.println("Okoa 100");
+		System.out.println("Okoa 50");
+		System.out.println("Okoa 20(or KSh50,Midnight)");
+		System.out.println("Okoa 10");
+		System.out.println("Okoa 5");
+		System.out.println("Okoa Internet");
+		System.out.println("More");					
+	}
+	
+	break;		
+		default:
+			
+			System.out.println("Invalid choice. Try again.");
+			
+		}
+
+	}
+
+}
